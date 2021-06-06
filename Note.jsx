9d9412372b53a438e93beea4abc306e20be14902
TanuGola <85428989+TanuGola@users.noreply.github.com>
2021-06-06 11:59:44 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note()
+{
+  return <header>
+    
+    <div className="note">
+    <h1>Javascript and react.js</h1>
+    <p>This was an amazing bootcamp. My name is Tanu Gola❤Verma Btech First year With CSE from GLA University</p></div>
+        </header>
+}
+export default Note;
